58f3aac6d9b7118059a2087509e8b28723ac5bd0
rockysingh2001 <103552722+rockysingh2001@users.noreply.github.com>
2024-02-22 20:56:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import CardContainer from './components/CardContainer'
+
+export default function App() {
+  return (
+    <>
+    <div className='container'>
+      
+      <h7 className='heading'>POKEMONS KINGDOM</h7>
+      
+      <CardContainer/>
+    </div>
+    </>
+  )
+}
